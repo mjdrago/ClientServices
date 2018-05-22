5477b2506698093a9d869df32a69040047d751e7
mjdrago <mario.j.drago@gmail.com>
2018-05-22 17:52:40 -0500

    Created ASP.NET project. Initiated Code First Migration and ran initial migration for standard User and Role tables.

---
+++ ClientServices/ClientServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClientServices.Startup))]
+namespace ClientServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
