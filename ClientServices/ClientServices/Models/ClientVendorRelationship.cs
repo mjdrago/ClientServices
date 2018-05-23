24b96183bcff07f554955143ac1452a078f45885
mjdrago <mario.j.drago@gmail.com>
2018-05-22 20:51:38 -0500

    Created Vendor and Vedor Contact Information.

---
+++ ClientServices/ClientServices/Models/ClientVendorRelationship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientServices.Models
+{
+    public class ClientVendorRelationship
+    {
+        public int Id { get; set; }
+
+        public string RelationshipDescription { get; set; }
+    }
+}
---
+++ ClientServices/ClientServices/Models/VendorContactPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientServices.Models
+{
+    public class VendorContactPerson:PersonContact
+    {
+        public int Id { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+        public int VendorId { get; set; }
+    }
+}
